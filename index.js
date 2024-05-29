79d52389e87d5380809fcb3c116229d604108d88
AlimGaziev <poshaalim@gmail.com>
2024-05-29 23:18:28 +0300

    create green square

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 let button = document.getElementById('btn')
 // debugger
 button.style.width = '200px'
+button.addEventListener('click', () => {
+    square.style.backgroundColor= 'blue'
+})
+let input = document.getElementById('name')
+input.value='Alim'
 
+let greenContainer= document.createElement('div')
+greenContainer.style.backgroundColor = 'green'
+greenContainer.style.width = '100px'
+greenContainer.style.height = '100px'
+
+let conteiner1=document.getElementById('conteiner')
+conteiner1.appendChild(greenContainer)
+conteiner1.style.border= '10px solid black'
